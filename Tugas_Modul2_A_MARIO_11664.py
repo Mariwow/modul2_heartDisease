d0f70fc2a62e3ae51c9deedb9b8569691f2c2d6d
Mariwow <220711664@students.uajy.ac.id>
2024-09-26 15:29:19 +0700

    Update Tugas_Modul2_A_MARIO_11664.py

--- Tugas_Modul2_A_MARIO_11664.py
+++ Tugas_Modul2_A_MARIO_11664.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pickle
 import os
 
-model = 'RF_heartDisease_model.pkl'
+model = 'GBT_heartDisease_model.pkl'
 
 with open(model,'rb') as f:
     loaded_model = pickle.load(f)
